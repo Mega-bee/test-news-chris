d459e6053e5277107271648198690c686e40b0c5
christianzakhour <christian.t.z@hotmail.com>
2022-05-27 11:08:58 +0300

    christian optimization and languages

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,47 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Arabic": MessageLookupByLibrary.simpleMessage("العربية"),
+        "English": MessageLookupByLibrary.simpleMessage("الانخليزية"),
+        "FROM": MessageLookupByLibrary.simpleMessage("من"),
+        "LOGIN": MessageLookupByLibrary.simpleMessage("ادخل ناطرينك"),
+        "NEWS": MessageLookupByLibrary.simpleMessage("الأخبار"),
+        "NEWS1": MessageLookupByLibrary.simpleMessage("الأخبار المباشرة"),
+        "No": MessageLookupByLibrary.simpleMessage("كلا"),
+        "NotavalidKey": MessageLookupByLibrary.simpleMessage("مفتاح غير صالح"),
+        "Popular": MessageLookupByLibrary.simpleMessage("المهمة"),
+        "Required": MessageLookupByLibrary.simpleMessage("مطلوب *"),
+        "SearchByDate":
+            MessageLookupByLibrary.simpleMessage("البحث حسب التاريخ"),
+        "Settings": MessageLookupByLibrary.simpleMessage("الاعدادات"),
+        "TO": MessageLookupByLibrary.simpleMessage("إلى"),
+        "WelcomeToLive": MessageLookupByLibrary.simpleMessage("مرحبا بكم في"),
+        "Yes": MessageLookupByLibrary.simpleMessage("نعم"),
+        "dark": MessageLookupByLibrary.simpleMessage("الاسود"),
+        "languages": MessageLookupByLibrary.simpleMessage("اللغة"),
+        "logout": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
+        "theme": MessageLookupByLibrary.simpleMessage("لون التطبيق"),
+        "white": MessageLookupByLibrary.simpleMessage("الابيض")
+      };
+}
